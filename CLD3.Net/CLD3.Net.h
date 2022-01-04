fe783f5ff0a4c54c3cf0e51511308285dc7177b8
Luis Miguel Soto Sánchez <luismiguelsoto@jerez.es>
2022-01-04 14:23:01 +0100

    Minor refactor

--- CLD3.Net/CLD3.Net.h
+++ CLD3.Net/CLD3.Net.h
@@ -3,13 +3,12 @@
 using namespace System;
 using namespace System::Text;
 
-namespace CLD3Net {
+namespace CLD3Net
+{
 	public ref class LanguageDetector
 	{
 	public:
 		String^ DetectLanguage(String^ text);
-	public:
 		String^ DetectNMostFreqLangs(String^ text, int numberOfLangs);
 	};
-
 }
